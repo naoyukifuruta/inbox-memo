9986ea88f56b7f5a1af09dc43f49cdbc8c80fa56
naoyukifuruta <naoyukifuruta@gmail.com>
2022-03-30 22:33:00 +0900

    lint対策

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+//import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:inbox_memo/providers/app_setting_provider.dart';
