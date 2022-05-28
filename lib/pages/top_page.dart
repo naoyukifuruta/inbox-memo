672a4d4792103e85eb1cd11693d0e28f23b2a909
naoyukifuruta <naoyukifuruta@gmail.com>
2022-05-28 20:01:47 +0900

    未使用import削除

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:inbox_memo/providers/app_setting_provider.dart';
