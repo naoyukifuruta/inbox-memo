9c896f1fd2cb5ca839c5535e246579666d0f820f
Naoyuki Furuta <nfrutta1208@gmail.com>
2022-06-08 15:48:28 +0900

    flutter3.0対応＆諸々修正

--- lib/providers/package_info_provider.dart
+++ lib/providers/package_info_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info/package_info.dart';
 
 final packageInfoProvider = Provider<PackageInfo>((ref) {
