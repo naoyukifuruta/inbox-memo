88a7caa4a8418c6b6c2d6746e57c42accf2be668
naoyukifuruta <naoyukifuruta@gmail.com>
2023-04-17 22:52:37 +0900

    mod

--- lib/providers/package_info_provider.dart
+++ lib/providers/package_info_provider.dart
@@ -1,6 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info/package_info.dart';
 
-final packageInfoProvider = Provider<PackageInfo>((ref) {
-  throw UnimplementedError();
-});
+final packageInfoProvider =
+    Provider<PackageInfo>((ref) => throw UnimplementedError());
