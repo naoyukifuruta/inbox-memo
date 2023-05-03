59585b65b9b068153d47ec81ac2930f36110bd36
naoyukifuruta <naoyukifuruta@gmail.com>
2023-05-03 17:28:08 +0900

    fix

--- lib/providers/memo_provider.dart
+++ lib/providers/memo_provider.dart
@@ -3,26 +3,26 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'shared_preferences_provider.dart';
 
 final memoProvider = StateNotifierProvider<MemoNotifier, String>((ref) {
-  return MemoNotifier(ref.read);
+  return MemoNotifier(ref);
 });
 
 class MemoNotifier extends StateNotifier<String> {
-  MemoNotifier(this._read) : super('') {
-    state = _read(sharedPreferencesProvider).getString('memo') ?? '';
+  MemoNotifier(this._ref) : super('') {
+    state = _ref.read(sharedPreferencesProvider).getString('memo') ?? '';
   }
 
-  final Reader _read;
+  final Ref _ref;
   late String _current;
   String get current => _current;
 
   void save(String text) {
-    _read(sharedPreferencesProvider).setString('memo', text);
+    _ref.read(sharedPreferencesProvider).setString('memo', text);
     state = text;
   }
 
   void clear() {
     _current = '';
-    _read(sharedPreferencesProvider).setString('memo', '');
+    _ref.read(sharedPreferencesProvider).setString('memo', '');
     state = '';
   }
 }
